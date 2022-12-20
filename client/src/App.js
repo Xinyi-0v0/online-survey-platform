9f63a8456ca1cbd9f8f711678ac10fc6a9361a0a
Xinyi Zhang <zhangxinyi@Xinyis-MacBook-Air.local>
2022-12-19 16:23:57 -0800

    finish Google OAuth

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          href="/auth/google"
+        >
+          sign in with google
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
